42ffcce0845fb3270c7bf979635f516ccf59bd43
VijayReddy-Devops <vijayreddykona666@gmail.com>
2026-08-16 19:57:11 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "vijay.aws.moonobucket"
+  bucket = "vijay.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -26,7 +26,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "ap-south-1"
-bucket = "vijay.aws.moonobucket"
+bucket = "vijay.bucket"
 key = "prod/terraform.tfstate"
 }
 }
