da716eb21c1bdf015bc67d8b740e87a6500d472a
VijayReddy-Devops <vijayreddykona666@gmail.com>
2026-08-16 21:23:06 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -26,3 +26,11 @@ resource "aws_s3_bucket_versioning" "three" {
     status = "Enabled"
   }
 }
+
+terraform {
+backend "s3" {
+region = "ap-south-1"
+bucket = "vijay-devops-app-bucket-2026"
+key = "prod/terraform.tfstate"
+}
+}
