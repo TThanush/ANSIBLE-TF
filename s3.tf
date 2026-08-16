02b278dafcacccde6c99b98ac611510716e5d50d
VijayReddy-Devops <vijayreddykona666@gmail.com>
2026-08-16 20:33:43 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,40 +1,28 @@
-resource "devops_s3_bucket" "abc" {
+resource "aws_s3_bucket" "abc" {
   bucket = "devops.aws.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "def" {
-  bucket = devops_s3_bucket.abc.id
+  bucket = aws_s3_bucket.abc.id
+
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
 resource "aws_s3_bucket_acl" "three" {
-  depends_on = [aws_s3_bucket_ownership_controls.def]
+  depends_on = [
+    aws_s3_bucket_ownership_controls.def
+  ]
 
-  bucket = devops_s3_bucket.abc.id
+  bucket = aws_s3_bucket.abc.id
   acl    = "private"
 }
 
 resource "aws_s3_bucket_versioning" "three" {
-bucket = devops_aws_bucket.abc.id
-versioning_configuration {
-status = "Enabled"
+  bucket = aws_s3_bucket.abc.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
-} 
-
-terraform {
-backend "s3" {
-region = "ap-south-1"
-bucket = "devops.aws.bucket"
-key = "prod/terraform.tfstate"
-}
-}
-
-
-
-
-
-
-
-
