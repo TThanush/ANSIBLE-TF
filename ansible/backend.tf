81b60d137b36e0cde37144b59a2cb6e1e1aaeb65
VijayReddy-Devops <vijayreddykona666@gmail.com>
2026-08-16 21:21:31 +0530

    Update backend.tf

--- ansible/backend.tf
+++ ansible/backend.tf
@@ -1,7 +1,8 @@
+
 terraform {
-  backend "s3" {
-    region = "ap-south-1"
-    bucket = "vijay-devops-app-bucket-2026"
-    key    = "prod/terraform.tfstate"
-  }
+backend "s3" {
+region = "ap-south-1"
+bucket = "vijay-devops-app-bucket-2026"
+key = "prod/terraform.tfstate"
+}
 }
