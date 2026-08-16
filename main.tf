8c841ac08320d5b88ec23aeda6779c1c0d27ab7a
VijayReddy-Devops <vijayreddykona666@gmail.com>
2026-08-16 16:23:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-02dfbd4ff395f2a1b"
+    image_id           = "ami-0d15e9052c94acb75"
     vpc_security_group_ids = [aws_security_group.web_server.id]
-    instance_type = "t3.micro"
-    key_name = "march-kp"
+    instance_type = "m7i-flex.large"
+    key_name = "Ansible"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-05b5c04b3e4f5fc97", "subnet-078eaa4691fce41d5"]
+     subnets = ["subnet-004784b75e90ce98e", "subnet-000ca19ee8985ae6a"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
@@ -33,7 +33,7 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1a", "us-east-1b"] 
+    availability_zones    = ["ap-south-1a", "ap-south-1c"] 
     launch_template {
         id      = aws_launch_template.web_server_as.id
         version = "$Latest"
