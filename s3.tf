eae3cffdd823f23a197cf6f5c447e2698b0aa866
VijayReddy-Devops <vijayreddykona666@gmail.com>
2026-08-16 20:22:46 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,23 +1,23 @@
-resource "aws_s3_bucket" "one" {
+resource "devops_s3_bucket" "abc" {
   bucket = "devops.aws.bucket"
 }
 
-resource "aws_s3_bucket_ownership_controls" "two" {
-  bucket = aws_s3_bucket.one.id
+resource "aws_s3_bucket_ownership_controls" "def" {
+  bucket = devops_s3_bucket.abc.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
 resource "aws_s3_bucket_acl" "three" {
-  depends_on = [aws_s3_bucket_ownership_controls.two]
+  depends_on = [aws_s3_bucket_ownership_controls.def]
 
-  bucket = aws_s3_bucket.one.id
+  bucket = devops_s3_bucket.abc.id
   acl    = "private"
 }
 
 resource "aws_s3_bucket_versioning" "three" {
-bucket = aws_s3_bucket.one.id
+bucket = devops_s3_bucket.abc.id
 versioning_configuration {
 status = "Enabled"
 }
