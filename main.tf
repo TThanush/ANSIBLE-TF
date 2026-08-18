e5ac2777099fd790c93d24292abafde617de4961
Thanush T <71646280+TThanush@users.noreply.github.com>
2026-08-18 07:30:42 +0530

    Update AWS launch template configuration

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0d15e9052c94acb75"
+    image_id           = "ami-0ac7b260cf76d8865"
     vpc_security_group_ids = [aws_security_group.web_server.id]
-    instance_type = "m7i-flex.large"
+    instance_type = "t3.micro"
     key_name = "Ansible"
     tags = {
-        Name = "DevOps"
+        env = "dev"
     }
     
 }
